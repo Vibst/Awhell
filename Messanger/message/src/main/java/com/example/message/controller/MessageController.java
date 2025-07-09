c5171075ce273f071cd46bafa899c53edfdc53b7
Vibst <131664611+Vibst@users.noreply.github.com>
2025-07-09 15:37:17 +0530

    basics operations in message Service and user Service is inislization

--- Messanger/message/src/main/java/com/example/message/controller/MessageController.java
+++ Messanger/message/src/main/java/com/example/message/controller/MessageController.java
@@ -1,5 +1,47 @@
 package com.example.message.controller;
 
+import java.sql.Time;
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.message.models.ChatMessage;
+
+@RestController
 public class MessageController {
-    
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public String sendMessage(ChatMessage chatMessage) {
+        System.out.println("Received message from " + chatMessage.getSender() + " to " + chatMessage.getReceiver()
+                + ": " + chatMessage.getContent());
+
+        chatMessage.setLocalDate(LocalDateTime.now());
+        ChatMessage sendMessage = getContent(chatMessage);
+        messagingTemplate.convertAndSend("/topic/message" + sendMessage.getReceiver(), sendMessage);
+        System.out.println("-----------------" + sendMessage.getLocalDate());
+
+        return "Successfully Send Message" + " " + sendMessage.getLocalDate();
+
+    }
+
+    public ChatMessage getContent(@RequestBody ChatMessage message) {
+        try {
+
+            // DB logics and Busness Logics Implements
+            Thread.sleep(2000);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error Are found In" + e.getMessage());
+        }
+        return message;
+
+    }
+
 }
---
+++ UerAuthenticateService/users/src/main/java/com/example/users/controller/userController.java
@@ -0,0 +1,15 @@
+package main.java.com.example.users.controller;
+
+@RestController
+public class userController {
+
+    public ResponseEntity<Users> authenticateUsers() {
+        try {
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+}
